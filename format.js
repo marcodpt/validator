721eb78294be0046e56d7089365094dfb1fc86d8
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2021-07-02 16:52:57 -0300

    Fix formatter

--- format.js
+++ format.js
@@ -8,13 +8,13 @@ export default {
     format: value => value.toLocaleString()
   },
   date: {
-    loader: value => new Date(value * 1000),
+    loader: value => new Date(value * 1000).toISOString(),
     parser: value => value ? parseInt(
       (new Date(value).getTime() / 1000).toFixed(0)
     ) : null,
     format: (value, type) => type == 'multipleOf' ?
       value.toLocaleString() :
-      value.toLocaleDateString()
+      new Date(value).toLocaleDateString()
   },
   number: {
     parser: value => isNaN(value) ? null : parseFloat(value),
